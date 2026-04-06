cd323a02d5d48531ddeec256e31bcdbc9dcea53c
Paola <paolarm.tij22@ustjr.edu.mx>
2026-04-06 12:11:51 -0600

    primer commit

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 const CACHE_NAME = 'capacitrek-cache-v1';
 const urlsToCache = [
-'/capacitrack/frontend/index.html',
+  '/capacitrack/frontend/index.html',
   '/capacitrack/frontend/assets/logo.png',
   '/capacitrack/frontend/assets/grupo-gusi.png',
   '/capacitrack/frontend/assets/esr.png'
@@ -26,32 +25,3 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
-=======
-const CACHE_NAME = 'capacitrek-cache-v1';
-const urlsToCache = [
-'/capacitrack/frontend/index.html',
-  '/capacitrack/frontend/assets/logo.png',
-  '/capacitrack/frontend/assets/grupo-gusi.png',
-  '/capacitrack/frontend/assets/esr.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => console.log('Archivos cacheados correctamente'))
-      .catch(err => console.error('Error al cachear:', err))
-  );
-});
-
-self.addEventListener('activate', event => {
-  console.log('Service Worker activado');
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
->>>>>>> bcac7488b91498c92bd39d5c7a0d11ac154d235f
